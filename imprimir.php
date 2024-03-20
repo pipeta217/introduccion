baf64e1fd7ed543c415ccd9f7a0815d0627a79ea
Luca <lucacristldo@gmail.com>
2024-03-20 08:58:16 -0300

    Nuevo archivo

---
+++ imprimir.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mi segundo archivo en PHP</h1>
+    <?php echo <<<END 
+        En un mundo contectado,
+        donde cada segundo se hacen al menos 100mil milanesas
+        electronicas... END; 
+    ?>
+</body>
+</html>
