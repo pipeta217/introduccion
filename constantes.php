370108596c411e972e115d3ba63b8fa80e0e4ff8
Felipe <pipeta217@gmail.com>
2024-04-10 08:20:40 -0300

    Constantes

---
+++ constantes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php 
+            //Forma 1
+            define('GRAVEDAD', 10.8);  
+            //Forma 2
+            //const GRAVEDAD2 = 9.8;
+            echo "Gravedad 1 ", $GRAVEDAD;
+            //echo "Gravedad 2 ", $GRAVEDAD2;
+            
+        ?>
+    </body>
+</html>
