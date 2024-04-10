b90257863be9abf00dc0dc1d240603a679069884
Felipe <pipeta217@gmail.com>
2024-04-10 08:52:52 -0300

    Ejercicio constantes PI

--- ejercicioPi.php
+++ ejercicioPi.php
@@ -10,7 +10,7 @@
         define('PI', 3.1416);
         const PI = 3.14;
         /*
-        1) Error: Warning: Constant PI already defined in C:\xampp\htdocs\introduccion\ejercicioPi.php on line 11
+        1) Error: Constant PI already defined in C:\xampp\htdocs\introduccion\ejercicioPi.php on line 11
         2) No es posible modificar el valor, puesto que el mismo es constante. 
         3) Para alamcenar valores que no cambien durante la ejecucion del programa.
         */    
