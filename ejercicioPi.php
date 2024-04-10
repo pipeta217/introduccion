7d320c48f3ff561325534dc16616cfd2f658aff3
Felipe <pipeta217@gmail.com>
2024-04-10 08:39:34 -0300

    Modificar constante

---
+++ ejercicioPi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        define('PI', 3.1416);
+        const PI = 3.14;
+        /*No es posible modificar el valor, puesto que el mismo es constante. 
+        Error: 
+        Warning: Constant PI already defined in C:\xampp\htdocs\introduccion\ejercicioPi.php on line 11
+        */    
+    ?>
+</body>
+</html>
