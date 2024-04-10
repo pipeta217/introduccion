4cf0b4eb296537a72bdf07f141f8566f11007af7
Felipe <pipeta217@gmail.com>
2024-04-10 08:46:02 -0300

    Declarar constantes

--- constantes.php
+++ constantes.php
@@ -6,10 +6,13 @@
         <title>Document</title>
     </head>
     <body>
+            <!--Dos formas de declararlas-->
         <?php
+            //Forma 1
             define('GRAVEDAD', 10.8);  
-            echo "La gravedad es: ", GRAVEDAD;
-            const GRAVEDAD = 9.8; 
+            //Forma 2
+            const GRAVEDAD2 = 9.8;
+            echo "La gravedad 1 es: ", GRAVEDAD, ", gravedad 2: ", GRAVEDAD2; 
         ?>
     </body>
 </html>
