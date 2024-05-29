11a6c475b0143ae3d8ed84a3f174bed9992f4768
Felipe <alumno@isbo.edu.uy>
2024-05-29 08:59:28 -0300

    Caracteres

--- ejercicioCaracteres.php
+++ ejercicioCaracteres.php
@@ -1,3 +1,9 @@
+<head>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="estilo.css">
+</head>
 <?php 
     echo "<h1>Cuento</h1>";
     $cuento="El dinosaurio (1959) es uno de los microcuentos más famosos dentro del género. Con él, inauguró la posibilidad de que con sólo una frase conformada por siete palabras se pueden aventurar diversas interpretaciones.
@@ -5,10 +11,14 @@
     Luego, si se analiza simbólicamente, el dinosaurio puede representar muchas cosas. Al pensar en la ambigüedad semántica, el hecho de que no sabe quién despertó y dónde es allí, permite establecer muchas opciones. En una visión existencialista, el dinosaurio puede encarnar la bestialidad de la existencia diaria, por lo que despertar se siente como una condena.
     Por otro lado, puede haber una posición política. Se puede considerar que el dinosaurio hace alusión al tipo de gobernante que no se retira jamás de su cargo y que actúa según pautas poco éticas. Este individuo puede identificarse con varios personajes en Latinoamérica. Es importante recordar que el autor tuvo problemas durante la dictadura en Guatemala y tuvo que exiliarse en México, por lo que muchas veces el tema formó parte de su obra.";
 
-    echo "<p>$cuento</p>";
+    echo "<p class='cuento'>$cuento</p>";
     $contador=explode(" ", $cuento);
-    echo "<h2>Condiciones</h2>";
-    
+    ?>
+
+<div class="padre">
+    <div class="hijo">
+<?php
+   echo "<h2>Condiciones</h2>";
     if(count($contador) <=100){
         echo "<p style= 'color:green'>Texto menor a 100 palabras</p>";
     }else{    
@@ -19,6 +29,6 @@
         echo "<p style= 'color:green'>Mayor a 700 caracteres</p>";
     }else{
         echo "<p style= 'color:red'>Mayor a 700 caracteres</p>";
-    }
-    
-?>
+    }?>
+    </div>
+</div>
