233ccb93283a7140bf798260cdb2d99646f08c02
Felipe <alumno@isbo.edu.uy>
2024-05-29 08:59:46 -0300

    Caracteres

--- cadenaDeCaracteres.php
+++ cadenaDeCaracteres.php
@@ -17,7 +17,10 @@
 <br>
 
 <?php 
-    $cuento="no sabe quién despertó y dóa. Se puede considerar que el dinosaurio hace alusión al tipo de gobernante que no se retira jamás de su cargo y que actúa según pautas poco éticas. Este individuo puede identificarse con varios personajes en Latinoamérica. Es importante recordar que el autor tuvo problemas durante la dictadura en Guatemala y tuvo que exiliarse en México, por lo que muchas veces el tema formó parte de su obra.";
+    $cuento="a Latina. El dinosaurio (1959) es uno de los microcuentos más famosos dentro del género. Con él, inauguró la posibilidad de que con sólo una frase conformada por siete palabras se pueden aventurar diversas interpretaciones.
+    En una primera lectura, se puede ver que pertenece al género fantástico. Un personaje tiene una pesadilla en la que aparece un dinosaurio. Sin embargo, cuando despierta, el animal prehistórico es real y se convierte en una posible amenaza. El lector sólo puede imaginar si el protagonista sentirá angustia y temor a una pronta muerte o si, por el contrario, se convertirá en una historia de aventuras. Se trata de un final abierto, por lo que resulta relativo.
+    Luego, si se analiza simbólicamente, el dinosaurio puede representar muchas cosas. Al pensar en la ambigüedad semántica, el hecho de que no sabe quién despertó y dónde es allí, permite establecer muchas opciones. En una visión existencialista, el dinosaurio puede encarnar la bestialidad de la existencia diaria, por lo que despertar se siente como una condena.
+    Por otro lado, puede haber una posición política. Se puede considerar que el dinosaurio hace alusión al tipo de gobernante que no se retira jamás de su cargo y que actúa según pautas poco éticas. Este individuo puede identificarse con varios personajes en Latinoamérica. Es importante recordar que el autor tuvo problemas durante la dictadura en Guatemala y tuvo que exiliarse en México, por lo que muchas veces el tema formó parte de su obra.";
     
     //Crea un array y guarda cada palabra en una posicion
     $contador=explode(" ", $cuento);
@@ -31,7 +34,7 @@
 
     echo "<br>";
 
-    //Strlen retorna la cantidad de caracteres de un String
+    //Strlen retorna la cantidad de caracteres de un String 
     if(strlen($cuento) > 700){
         echo "Supera los 700 caracteres";
     }else{
